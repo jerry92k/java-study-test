1ddbaecd9a06561928ce4dd734354a9adc71b3c1
jerry92k <jens92.k@gmail.com>
2022-01-12 23:26:18 +0900

    test: Array, ArrayList, LinkedList 속도 비교 테스트 코드 보완

---
+++ src/main/java/study/array/RandomArray.java
@@ -0,0 +1,24 @@
+package study.array;
+
+import java.util.Random;
+
+public class RandomArray {
+
+	private static Random random = new Random();
+	public static String[] makeStrs(int strLen, int arraySize){
+
+		String[] strs= new String[arraySize];
+		for(int i=0; i<arraySize; i++){
+			strs[i] =makeWord(strLen);
+		}
+		return strs;
+	}
+
+	private static String makeWord(int strLen) {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<strLen; i++){
+			sb.append((char) (random.nextInt(26) + 97));
+		}
+		return sb.toString();
+	}
+}
--- src/test/java/study/array/ArrayTest.java
+++ src/test/java/study/array/ArrayTest.java
@@ -3,47 +3,114 @@ package study.array;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ArrayTest {
 
-	@Test
-	void searchInArray(){
-		String[] strs= {"a","b","c","d","e","f","g","h","i","j","k","l"};
-		long startTime = System.nanoTime();
-		for(String str: strs){
-			if(str.equals("l")){
-				long endTime = System.nanoTime() - startTime;
-				System.out.println("times : "+endTime);
-				break;
-			}
-		}
+	private static final Random random = new Random();
+	private static final int arraySize = 1_000_000;
+	private static final int strLen = 6;
+	private static final int times = 1_000_000;
+	private static String[] strs;
+
+	@BeforeAll
+	public static void setUp() {
+		strs = RandomArray.makeStrs(strLen, arraySize);
 	}
 
 	@Test
-	void searchInArrayList(){
-		List<String> strs = Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l");
-		long startTime = System.nanoTime();
-		for(String str: strs){
-			if(str.equals("l")){
-				long endTime = System.nanoTime() - startTime;
-				System.out.println("times : "+endTime);
-				break;
+	void compareSearchTime() {
+		List<String> listStrs = Arrays.asList(strs);
+		List<String> linkedStrs = new LinkedList<>(Arrays.asList(strs));
+
+		long arrayTotalTime = 0;
+		long arrayListTotalTime = 0;
+		long linkedListTotalTime = 0;
+
+		int halfTimes=times/2;
+
+		for (int i = 0; i < times; i++) {
+			String target = strs[random.nextInt(arraySize)];
+
+			long startTime = System.nanoTime();
+			for(int j=0; j<strs.length; j++){
+				if (strs[j].equals(target)) {
+					break;
+				}
+			}
+			if(i>halfTimes){
+				arrayTotalTime += System.nanoTime() - startTime;
+			}
+
+
+			startTime = System.nanoTime();
+			listStrs.indexOf(target);
+			if(i>halfTimes){
+				arrayListTotalTime += System.nanoTime() - startTime;
+			}
+
+
+			startTime = System.nanoTime();
+			linkedStrs.indexOf(target);
+			if(i>halfTimes){
+				linkedListTotalTime += System.nanoTime() - startTime;
 			}
 		}
+
+		System.out.println("Array times : "+ (double)arrayTotalTime / halfTimes);
+		System.out.println("ArrayList times : "+ (double)arrayListTotalTime / halfTimes);
+		System.out.println("LinkedList times : "+ (double)linkedListTotalTime / halfTimes);
 	}
 
 	@Test
-	void searchInLinkedList(){
-		List<String> strs=new LinkedList<>(Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l"));
-		long startTime = System.nanoTime();
-		for(String str: strs){
-			if(str.equals("l")){
-				long endTime = System.nanoTime() - startTime;
-				System.out.println("times : "+endTime);
-				break;
+	void searchInArray() {
+		long totalTime = 0;
+
+		for (int i = 0; i < times; i++) {
+			String target = strs[random.nextInt(arraySize)];
+
+			long startTime = System.nanoTime();
+			for (String str : strs) {
+				if (str.equals(target)) {
+					break;
+				}
 			}
+			long endTime = System.nanoTime() - startTime;
+			totalTime += endTime;
 		}
+		System.out.println((double)totalTime / times);
+	}
+
+	@Test
+	void searchInArrayList() {
+		List<String> listStrs = Arrays.asList(strs);
+
+		long totalTime = 0;
+		for (int i = 0; i < times; i++) {
+			String target = strs[random.nextInt(arraySize)];
+			long startTime = System.nanoTime();
+			listStrs.indexOf(target);
+			long endTime = System.nanoTime() - startTime;
+			totalTime += endTime;
+		}
+		System.out.println((double)totalTime / times);
+	}
+
+	@Test
+	void searchInLinkedList() {
+		List<String> listStrs = new LinkedList<>(Arrays.asList(strs));
+
+		long totalTime = 0;
+		for (int i = 0; i < times; i++) {
+			String target = strs[random.nextInt(arraySize)];
+			long startTime = System.nanoTime();
+			listStrs.indexOf(target);
+			long endTime = System.nanoTime() - startTime;
+			totalTime += endTime;
+		}
+		System.out.println((double)totalTime / times);
 	}
 }
