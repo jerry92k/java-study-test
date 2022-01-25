3ad1cb83b8ddbf93142b4541d00c5386d6034446
jerry92k <jens92.k@gmail.com>
2022-01-25 12:55:12 +0900

    feat: 상위클래스 생성자가 private 경우 상속 오류

---
+++ src/main/java/study/utils/CarUtils.java
@@ -0,0 +1,12 @@
+package study.utils;
+
+public class CarUtils {
+
+	private CarUtils(){
+
+	}
+
+	public static void doTuning(){
+		System.out.println("do tuning");
+	}
+}
---
+++ src/main/java/study/utils/SubCar.java
@@ -0,0 +1,5 @@
+package study.utils;
+// There is no default constructor available in 'study.utils.CarUtils'
+// public class SubCar extends CarUtils{
+//
+// }
